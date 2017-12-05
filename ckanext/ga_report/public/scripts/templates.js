8b3a20edcc06fa801af0c1f6c4d97158b5c8288c
lrudenko <lilia.rudenko@linkdigital.com.au>
2017-12-05 18:10:42 +0200

    SXSAGXZFZ-383 / templates.js fixes

--- ckanext/ga_report/public/scripts/templates.js
+++ ckanext/ga_report/public/scripts/templates.js
@@ -21,8 +21,8 @@ CKAN.Templates.resourceUpload = ' \
 CKAN.Templates.resourceEntry = ' \
   <li class="ui-state-default resource-edit drag-bars"> \
     <a class="resource-open-my-panel" href="#">\
-      <img class="js-resource-icon inline-icon resource-icon" src="${resource_icon}" /> \
-      <span class="js-resource-edit-name">${resource.name}</span>\
+      <img class="js-resource-icon inline-icon resource-icon" src="{{resource_icon}}" /> \
+      <span class="js-resource-edit-name">{{resource.name}}</span>\
     </a>\
   </li>';
 
@@ -38,7 +38,7 @@ CKAN.Templates.resourceDetails = ' \
     <div class="control-group"> \
       <label for="" class="control-label" property="rdfs:label">'+CKAN.Strings.name+'</label> \
       <div class="controls" property="rdf:value"> \
-        <input class="js-resource-edit-name" name="resources__${num}__name" type="text" value="${resource.name}" class="long" /> \
+        <input class="js-resource-edit-name" name="resources__{{num}}__name" type="text" value="{{resource.name}}" class="long" /> \
       </div> \
     </div> \
     <div class="control-group"> \
@@ -50,91 +50,91 @@ CKAN.Templates.resourceDetails = ' \
             <li><button class="btn js-markdown-preview">'+CKAN.Strings.preview+'</button></li> \
           </ul> \
           <div> \
-            <textarea class="js-resource-edit-description markdown-input" name="resources__${num}__description">${resource.description}</textarea> \
+            <textarea class="js-resource-edit-description markdown-input" name="resources__{{num}}__description">{{resource.description}}</textarea> \
           </div> \
           <div class="markdown-preview" style="display: none;"></div> \
           <span class="hints">'+youCanUseMarkdownString+'</span> \
         </div> \
       </div> \
     </div> \
-    <div class="control-group {{if resource.url_error}} error{{/if}}"> \
+    <div class="control-group {% if resource.url_error %} error{% endif %}"> \
       <label for="" class="control-label" property="rdfs:label">'+CKAN.Strings.url+'</label> \
       <div class="controls"> \
-        {{if resource.resource_type=="file.upload"}} \
-          ${resource.url} \
-          <input name="resources__${num}__url" type="hidden" value="${resource.url}" /> \
-        {{/if}} \
-        {{if resource.resource_type!="file.upload"}} \
-          <input name="resources__${num}__url" type="text" value="${resource.url}" class="long" title="${resource.url_error}" /> \
-        {{/if}} \
+        {% if resource.resource_type=="file.upload" %} \
+          {{resource.url}} \
+          <input name="resources__${num}__url" type="hidden" value="{{resource.url}}" /> \
+        {% endif %} \
+        {% if resource.resource_type!="file.upload" %} \
+          <input name="resources__${num}__url" type="text" value="{{resource.url}}" class="long" title="{{resource.url_error}}" /> \
+        {% endif %} \
       </div> \
     </div> \
     <div class="control-group"> \
       <label for="" class="control-label" property="rdfs:label">'+CKAN.Strings.format + '\
-          &nbsp;&nbsp;<img class="js-resource-icon inline-icon resource-icon" src="${resource_icon}" /> </label>\
+          &nbsp;&nbsp;<img class="js-resource-icon inline-icon resource-icon" src="{{resource_icon}}" /> </label>\
       <div class="controls"> \
-        <input name="resources__${num}__format" type="text" value="${resource.format}" class="long js-resource-edit-format autocomplete-format" placeholder="'+CKAN.Strings.resourceFormatPlaceholder+'" /> \
+        <input name="resources__{{num}}__format" type="text" value="{{resource.format}}" class="long js-resource-edit-format autocomplete-format" placeholder="'+CKAN.Strings.resourceFormatPlaceholder+'" /> \
       </div> \
     </div> \
     <div class="control-group"> \
       <label for="" class="control-label" property="rdfs:label">'+CKAN.Strings.resourceType+'</label> \
       <div class="controls"> \
-        {{if resource.resource_type=="file.upload"}} \
+        {% if resource.resource_type=="file.upload" %} \
           '+CKAN.Strings.dataFileUploaded+' \
-          <input name="resources__${num}__resource_type" type="hidden" value="${resource.resource_type}" /> \
-        {{/if}} \
-        {{if resource.resource_type!="file.upload"}} \
-          <select name="resources__${num}__resource_type" class="short"> \
-            {{each resourceTypeOptions}} \
-            <option value="${$value[0]}" {{if $value[0]==resource.resource_type}}selected="selected"{{/if}}>${$value[1]}</option> \
-            {{/each}} \
+          <input name="resources__{{num}}__resource_type" type="hidden" value="{{resource.resource_type}}" /> \
+        {% endif %} \
+        {% if resource.resource_type!="file.upload" %} \
+          <select name="resources__{{num}}__resource_type" class="short"> \
+            {% for res in resourceTypeOptions %} \
+            <option value="{{$value[0]}}" {% if $value[0]==resource.resource_type %}selected="selected"{% endif %}>{{$value[1]}}</option> \
+            {% endfor %} \
           </select> \
-        {{/if}} \
+        {% endif %} \
       </div> \
     </div> \
     <div class="control-group"> \
       <label for="" class="control-label" property="rdfs:label">'+CKAN.Strings.lastModified+'</label> \
       <div class="controls"> \
-        <input class="input-small" name="resources__${num}__last_modified" type="text" value="${resource.last_modified}" /> \
+        <input class="input-small" name="resources__{{num}}__last_modified" type="text" value="{{resource.last_modified}}" /> \
         <div class="hint">'+datesAreInISOString+'</div> \
       </div> \
     </div> \
     <div class="control-group"> \
       <label for="" class="control-label" property="rdfs:label">'+CKAN.Strings.sizeBracketsBytes+'</label> \
       <div class="controls"> \
-        <input name="resources__${num}__size" type="text" value="${resource.size}" class="long" /> \
+        <input name="resources__{{num}}__size" type="text" value="{{resource.size}}" class="long" /> \
       </div> \
     </div> \
     <div class="control-group"> \
       <label for="" class="control-label" property="rdfs:label">'+CKAN.Strings.mimetype+'</label> \
       <div class="controls"> \
-        <input name="resources__${num}__mimetype" type="text" value="${resource.mimetype}" /> \
+        <input name="resources__{{num}}__mimetype" type="text" value="{{resource.mimetype}}" /> \
       </div> \
     </div> \
     <div class="control-group"> \
       <label for="" class="control-label" property="rdfs:label">'+CKAN.Strings.mimetypeInner+'</label> \
       <div class="controls"> \
-        <input name="resources__${num}__mimetype_inner" type="text" value="${resource.mimetype_inner}" /> \
+        <input name="resources__{{num}}__mimetype_inner" type="text" value="{{resource.mimetype_inner}}" /> \
       </div> \
     </div> \
     <div class="control-group"> \
       <label for="" class="control-label" property="rdfs:label">'+CKAN.Strings.id+'</label> \
       <div class="controls"> \
-        <input type="text" disabled="disabled" value="${resource.id}" class="disabled" /> \
-        <input name="resources__${num}__id" type="hidden" value="${resource.id}" /> \
+        <input type="text" disabled="disabled" value="{{resource.id}}" class="disabled" /> \
+        <input name="resources__{{num}}__id" type="hidden" value="{{resource.id}}" /> \
       </div> \
     </div> \
     <div class="control-group"> \
       <label for="" class="control-label" property="rdfs:label">'+CKAN.Strings.hash+'</label> \
       <div class="controls"> \
-        <input type="text" disabled="disabled" class="disabled" value="${resource.hash || "'+CKAN.Strings.unknown+'"}"/> \
-        <input name="resources__${num}__hash" type="hidden" value="${resource.hash}" /> \
+        <input type="text" disabled="disabled" class="disabled" value="{{resource.hash or "'+CKAN.Strings.unknown+'"}}"/> \
+        <input name="resources__{{num}}__hash" type="hidden" value="{{resource.hash}}" /> \
       </div> \
     </div> \
     <div class="control-group"> \
       <label for="" class="control-label" property="rdfs:label">'+CKAN.Strings.created+'</label> \
       <div class="controls"> \
-        <input type="text" disabled="disabled" value="${resource.created}" class="disabled" /> \
+        <input type="text" disabled="disabled" value="{{resource.created}}" class="disabled" /> \
       </div> \
     </div> \
     <div class="control-group"> \
@@ -152,7 +152,7 @@ CKAN.Templates.resourceDetails = ' \
 CKAN.Templates.resourceExtra = ' \
   <div class="dynamic-extra"> \
   <button class="btn btn-danger remove-resource-extra">X</button>\
-  <input type="text" placeholder="'+CKAN.Strings.key+'" class="extra-key" value="${key}" /> \
-  <input type="text" placeholder="'+CKAN.Strings.value+'" class="extra-value" value="${value}" /> \
+  <input type="text" placeholder="'+CKAN.Strings.key+'" class="extra-key" value="{{key}}" /> \
+  <input type="text" placeholder="'+CKAN.Strings.value+'" class="extra-value" value="{{value}}" /> \
   </div> \
   ';
